6f4e9ea8bd50fb53ce889bda72c07a4755c06426
bake0937 <sadayasu.info@gmail.com>
2020-06-28 12:25:08 +0900

    プルリクエストをopen close した時のLabelの追加・削除の処理を設定

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
-import { Application } from "probot"; // eslint-disable-line no-unused-vars
+import { Application } from "probot";
 
 export = (app: Application) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  // Pull Requestが作成されたタイミングで呼び出されます
+  app.on("pull_request.opened", async (context) => {
+    // ラベル追加APIを呼び出す際のリクエストパラメータ生成
+    const open_add_labels_params = context.issue({
+      labels: ["WIP"],
     });
-    await context.github.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    // 作成された Pull Requestにラベル追加
+    await context.github.issues.addLabels(open_add_labels_params);
+  });
+
+  app.on("pull_request.closed", async (context) => {
+    // ラベル削除APIを呼び出す際のリクエストパラメータ生成
+    const labels: Array<string> = ["WIP", "Review"];
+    // close するためラベルを削除
+    labels.forEach((label) => {
+      const remove_labels_params = context.issue({
+        name: label,
+      });
+      context.github.issues.removeLabel(remove_labels_params);
+    });
+
+    // Reviewd を貼る
+    const close_add_labels_params = context.issue({
+      labels: ["Reviewd"],
+    });
+    context.github.issues.addLabels(close_add_labels_params);
+  });
 };
