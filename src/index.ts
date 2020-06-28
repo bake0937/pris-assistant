b3c382455bd3e4df3992bbe74995d91a2d615fb9
bake0937 <sadayasu.info@gmail.com>
2020-06-28 14:40:17 +0900

    定数でLabelの追加，作業をできるようにした

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Application } from "probot";
 export = (app: Application) => {
   // Pull Requestが作成されたタイミングで呼び出されます
   app.on("pull_request.opened", async (context) => {
+    const open_add_labels: Array<string> = process.env.OPEN_ADD_LABELS as any;
     // ラベル追加APIを呼び出す際のリクエストパラメータ生成
     const open_add_labels_params = context.issue({
-      labels: ["WIP"],
+      labels: open_add_labels,
     });
 
     // 作成された Pull Requestにラベル追加
@@ -14,18 +15,19 @@ export = (app: Application) => {
 
   app.on("pull_request.closed", async (context) => {
     // ラベル削除APIを呼び出す際のリクエストパラメータ生成
-    const labels: Array<string> = ["WIP", "Review"];
+    const close_remove_labels: string = process.env.CLOSE_REMOVE_LABELS as any;
     // close するためラベルを削除
-    labels.forEach((label) => {
+    close_remove_labels.split(",").forEach((close_remove_label) => {
       const remove_labels_params = context.issue({
-        name: label,
+        name: close_remove_label,
       });
       context.github.issues.removeLabel(remove_labels_params);
     });
 
     // Reviewd を貼る
+    const close_add_labels: Array<string> = process.env.CLOSE_ADD_LABELS as any;
     const close_add_labels_params = context.issue({
-      labels: ["Reviewd"],
+      labels: close_add_labels,
     });
     context.github.issues.addLabels(close_add_labels_params);
   });
